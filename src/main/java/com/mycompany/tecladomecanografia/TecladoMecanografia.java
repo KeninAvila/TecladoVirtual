43141e6d66e637dfdd4cd82ad2ce58ce445e22fb
KeninAvila <149337855+KeninAvila@users.noreply.github.com>
2024-01-13 22:46:43 -0500

    Punto 1

---
+++ src/main/java/com/mycompany/tecladomecanografia/TecladoMecanografia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tecladomecanografia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TecladoMecanografia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
